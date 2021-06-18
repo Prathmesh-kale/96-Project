6eb565cb5fa58e809385abc6ec0a9c8799908ff5
Prathmesh-kale <79739726+Prathmesh-kale@users.noreply.github.com>
2021-06-18 14:10:32 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,75 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDtKsQH8eBRxMB_MgRRxo6g8PRjwrxB9lo",
+    authDomain: "kwitter01-9b7da.firebaseapp.com",
+    databaseURL: "https://kwitter01-9b7da-default-rtdb.firebaseio.com",
+    projectId: "kwitter01-9b7da",
+    storageBucket: "kwitter01-9b7da.appspot.com",
+    messagingSenderId: "671642016052",
+    appId: "1:671642016052:web:9792dacaac776b3a142d61"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  user_name = localStorage.getItem("user_name");
+  room_name = localStorage.getItem("room_name");
+
+  function send()
+{
+    msg = document.getElementById("msg").value;
+    firebase.database().ref(room_name).push({name : user_name, message : msg, like : 0});
+    document.getElementById("msg").value = "";
+}
+
+function getData()
+{
+  firebase.database().ref("/" + room_name).on('value' , function (snapshot)
+  {
+    document.getElementById("output").innerHTML = "";
+    snapshot.forEach(function (childSnapshot)
+    {
+      childKey = childSnapshot.key;
+      childData = childSnapshot.val();
+      if(childKey != "purpose")
+      {
+        firebase_message_id = childKey;
+        message_data = childData;
+        console.log(firebase_message_id);
+        console.log(message_data);
+        name = message_data['name'];
+        message = message_data['message'];
+        like = message_data['like'];
+        name_with_tag = "<h4>" + name + "<img class = 'user_tick' src = 'tick.png'></h4>";
+        message_with_tag = "<h4 class = 'message_h4'>" + message + "</h4>";
+        like_button = "<button class = 'btn btn-warning' id = " + firebase_message_id + " value = " + like + " onclick = 'updateLike(this.id)'>";
+        span_with_tag = "<span class = 'glyphicon glyphicon-thumbs-up'> Like: " + like + "</span></button><hr>";
+
+        row = name_with_tag + message_with_tag + like_button + span_with_tag;
+        document.getElementById("output").innerHTML += row;
+      }
+    });
+  });
+}
+getData();
+
+function updateLike(message_id)
+{
+  console.log("Clicked on like button - " + message_id);
+  button_id = message_id;
+  likes = document.getElementById(button_id).value;
+  updated_likes = Number(likes)+1;
+  console.log(updated_likes);
+
+  firebase.database().ref(room_name).child(message_id).update(
+    {
+      like: updated_likes
+    }
+  );
+}
+
+function logout()
+{
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+
+  window.location.replace("index.html");
+}
